ac6b05f0c3f10290240676c2983a47538b3627bb
XCodeRoot <1045547784@qq.com>
2023-11-08 23:24:49 +0800

    使用Sharding-JDBC实现程序的读写分类(mysql)

--- src/test/java/com/atguigu/test.java
+++ src/test/java/com/atguigu/test.java
@@ -9,17 +9,8 @@ import java.util.concurrent.atomic.LongAdder;
 public class test {
 
     @Test
-    public void test1(){
+    public void test1() {
 
-        /*
-        TODO:
-            1. E-R图 : 顾客,订单,订单细节 实体
-            2. 数据流图 : 顾客实体
-                        顾客查询订单
-                        顾客填写订单 - 取消订单
-                        店员查看订单
-            3. 模块图 :
-         */
 
     }
 }
